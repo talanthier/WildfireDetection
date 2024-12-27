6a97c410a4c9786cb15d7ab90eecd07809fc5fee
Tim Lanthier <timlanthier@Tims-Air.localdomain>
2024-12-26 20:16:00 -0800

    Added modeling_03 notebook and saved best model after 30 epochs. Added __init__.py to keep track of model classes. We will use model_class.py for importing from state dicts

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,5 @@
-import model_class
+# Define the __all__ variable
+__all__ = ["model_class"]
+
+# Import the submodules
+from . import model_class
--- models/model_class.py
+++ models/model_class.py
@@ -55,4 +55,29 @@ class Model_1(nn.Module):
         x = self.relu3(self.fc1(x))
         x = self.sigmoid(self.fc2(x))
         return x
+
+class Model_2(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        
+        # Define the layers
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(32*16*16, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Define the forward pass using the layers defined in __init__
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = self.flatten(x)
+        x = self.sigmoid(self.fc1(x))
+        return x
     
